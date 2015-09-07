82a0042663b55defa0f93c00d1a24e62aa4d2440
Michael Valdez <michaelvaldez@Michaels-MacBook-Air.local>
2015-09-07 11:38:54 -0600

    First commit for completing PA1

--- Point.cpp
+++ Point.cpp
@@ -1,17 +1,20 @@
 #include "Point.h"
+#include <cmath>
 
 // Default constructor
-// Initializes the point to (0.0, 0.0)
+// Initializes the point to (0.0, 0.0, 0.0)
 Point::Point() {
   x = 0.0;
   y = 0.0;
+  z = 0.0;
 }
 
 // Constructor
-// Initializes the point to (initX, initY)
-Point::Point(double initX, double initY) {
+// Initializes the point to (initX, initY, initZ)
+Point::Point(double initX, double initY, double initZ) {
   x = initX;
   y = initY;
+  z = initZ;
 }
 
 // Destructor
@@ -30,6 +33,9 @@ void Point::setX(double newX) {
 void Point::setY(double newY) {
   y = newY;
 }
+void Point::setZ(double newZ) {
+  z = newZ;
+}
 
 // Accessors
 // Return the current values of private member variables
@@ -40,4 +46,19 @@ double Point::getX() {
 
 double Point::getY() {
   return y;
+}
+
+double Point::getZ() {
+  return z;
+}
+
+// Gets a distance to another point
+// Returns the distance as a double
+
+double Point::distanceTo(Point & pt) {
+  double distance = 0.0;
+  
+  distance = sqrt(pow((getX() - pt.getX()),2) + pow((getY() - pt.getY()),2) + pow((getZ() - pt.getZ()),2));
+
+  return distance;
 }
--- Point.h
+++ Point.h
@@ -9,11 +9,12 @@ class Point {
 private:
   double x;
   double y;
+  double z;
 
 public:
   // Constructors
   Point();                      // default constructor
-  Point(double x, double y);    // two-argument constructor
+  Point(double x, double y, double z);    // two-argument constructor
 
   // Destructor
   ~Point();
@@ -21,10 +22,15 @@ public:
   // Mutator methods
   void setX(double newX);
   void setY(double newY);
+  void setZ(double newZ);
 
   // Accessor methods
   double getX();
   double getY();
+  double getZ();
+
+  // function to get a distance to another point
+  double distanceTo(Point & pt);
 };
 
 #endif // __point_h
